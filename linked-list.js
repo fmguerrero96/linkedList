b11b0a9b8e13f33f2ca80e2af1cb8fe59b44b735
Misael Guerrero <mguerrerowest@gmail.com>
2023-04-29 22:06:01 -0400

    create prepend function which adds a new node to the start of the list

--- linked-list.js
+++ linked-list.js
@@ -1,8 +1,33 @@
 const Node = (value = null, nextNode = null) => {
-    return {value, nextNode}
-}
+    return {value, nextNode};
+};
 
 const LinkedList = () => {
-    let head = null
-    let length = 0 
-}
+    let head = null;
+    let length = 0; 
+
+    const append = (value) => {
+        let newNode = Node(value);
+        if (head === null) {
+            head = newNode
+        } else {
+            let currentNode = head 
+            while(currentNode.nextNode !== null){   //while there is a next node,
+                currentNode = currentNode.nextNode; //hop to the nex node
+            }
+            currentNode.nextNode = newNode          //add node to end of list
+        }
+        length++
+    }
+
+    const prepend = (value) => {
+        let newNode = Node(value, head);
+        head = newNode
+        length++
+    }
+
+    return {
+        append,
+        prepend
+    }
+};
